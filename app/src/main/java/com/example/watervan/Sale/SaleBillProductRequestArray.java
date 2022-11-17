129501ce1199af544ce4a23bb05ea896b5434400
Jissa988 <jisssa398@gmail.com>
2022-11-17 18:28:49 +0530

    dg

---
+++ app/src/main/java/com/example/watervan/Sale/SaleBillProductRequestArray.java
@@ -0,0 +1,17 @@
+package com.example.watervan.Sale;
+
+import java.util.List;
+
+public class SaleBillProductRequestArray {
+    List<SaleBillProductRequest> TypeSaleLine;
+
+    public List<SaleBillProductRequest> getTypeSaleLine() {
+        return TypeSaleLine;
+    }
+
+    public void setTypeSaleLine(List<SaleBillProductRequest> typeSaleLine) {
+        TypeSaleLine = typeSaleLine;
+    }
+
+
+}
